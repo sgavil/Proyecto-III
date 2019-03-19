9fcc76aa81d49dc086503aed5dc202eb4b50f1b2
Ramón <ramonarj@ucm.es>
2019-03-19 12:17:51 +0100

    Creadas carpetas nuevas y proyecto de test para la IA

---
+++ Src/AITest/Game.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <Arquitectura/physicSystem.h>
+#include <Scenes/SceneManager.h> 
+#include <Arquitectura\OgreSystem.h>
+#include <OgreStringVector.h>
+#include <Arquitectura/Entity.h>
+#include <string>
+#include <SDL.h>
+#include <SDL_video.h>
+#include <AudioSource/AudioSource.h>
+#include "vld.h"
+#include <Arquitectura/Components.h>
+
+class Rigidbody;
+class Game {
+public:
+	Game(std::string basicConfig);
+	~Game();
+
+	void start();
+	void run();
+private:
+	physicSystem* physSyst_;
+	SceneManager* ScnMng_;
+	OgreSystem* ogreSyst_; //Informaci�n inicial de Ogre
+	AudioSource* audioSrc_;
+	Rigidbody* floorRigidbody;
+
+	bool exit;
+};
---
+++ Src/Lógica/NPC.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "Arquitectura/Component.h"
+
+class NPC : public Component
+{
+public:
+	NPC();
+	~NPC();
+
+	void load(json file);
+	virtual void render(unsigned int time) {};
+	virtual void update(unsigned int time) {};
+	virtual bool handleEvent(SDL_Event* e, unsigned int time) { return false; };
+	virtual void receive(Message* msg) {};
+
+private:
+	int hunger;
+	int thirst;
+};
