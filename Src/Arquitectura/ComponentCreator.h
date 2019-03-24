12795aa994c1377bf7a0a87cfed5c42bbfac00a2
Raúl Fernández Guardia <raulfe06@ucm.es>
2019-03-24 12:24:45 +0100

    Explicado código de la macro y arreglada ejecución en Debug

--- Src/Arquitectura/ComponentCreator.h
+++ Src/Arquitectura/ComponentCreator.h
@@ -2,15 +2,22 @@
 #include "EntityFactory.h"
 
 // ·> Macro para el registro de factorías de componentes
-#define REGISTER_TYPE(klass) \
-    class klass##Creator : public BaseCreator { \
+/*
+- #: Convierte el nombre del parámetro en un <String> -> Si Comp = Transform, entonces registerType("Transform", this)
+- ##: Sirve para concatenar el nombre ->  Si Comp = Transform, entonces class TransformCreator, etc.
+
+Usamos una variable 'static' porque la constructora de los objetos 'static' se llama antes de la llamada la main().
+De esta manera, todas las factorías de componentes quedan registradas automáticamente antes de la ejecución del programa en sí.
+*/
+#define REGISTER_TYPE(Comp) \
+    class Comp##Creator : public BaseCreator { \
     public: \
-        klass##Creator() \
+        Comp##Creator() \
         { \
-            EntityFactory::registerType(#klass, this); \
+            EntityFactory::registerType(#Comp, this); \
         } \
        virtual Component* createComponent() const { \
-            return new klass(); \
+            return new Comp(); \
         } \
     }; \
-    static klass##Creator global_##klass##Creator;
+    static Comp##Creator global_##Comp##Creator;
