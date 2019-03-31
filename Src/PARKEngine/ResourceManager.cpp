2bf214420c43a97e807f2385286e4ca20db6456f
Antonio Cardona Costa <antocard@ucm.es>
2019-03-31 17:38:56 +0200

    CEGUI - Test metiendo elementos como botones

--- Src/PARKEngine/ResourceManager.cpp
+++ Src/PARKEngine/ResourceManager.cpp
@@ -106,17 +106,57 @@ void ResourceManager::ceguiInit() {
 	CEGUI::System::getSingleton().getDefaultGUIContext().setDefaultTooltipType("TaharezLook/Tooltip");
 
 	
-	//Crear ventana cegui de prueba
-	CEGUI::WindowManager& wmgr = CEGUI::WindowManager::getSingleton();
-	CEGUI::Window* myRoot = wmgr.createWindow("DefaultWindow", "root");
-	CEGUI::System::getSingleton().getDefaultGUIContext().setRootWindow(myRoot);
+	///////////////////////////////
+	// ARRANCANDO INTERFAZ
+	//	- Existen dos formas de hacerlo: Programando la interfaz directamente en c++ o cargandola completa de un archivo xml (layout)
+	///////////////////////////////
+
+	//////////////////////////////
+	// USANDO C++
+	//////////////////////////////
+
+	//Crear ventana Invisible sobre la que construir el interfaz
+	CEGUI::WindowManager& wmgr = CEGUI::WindowManager::getSingleton();		// Obtenemos la ventana de renderizado
+	CEGUI::Window* myRoot = wmgr.createWindow("DefaultWindow", "root");		// Creamos una ventana de interfaz con parametros (Tipo ventana definida en Scheme activo, nombre asignado a la ventana)
+	CEGUI::System::getSingleton().getDefaultGUIContext().setRootWindow(myRoot); // Establece que sets de GUI se muestra en el contexto actual, puede cambiarse de uno a otro.
+
+	//Hasta este punto ya se ha creado la ventana invisible sobre la que pondremos los widgets que formaran el menu
+	//Vamos a crear elementos sobre esa ventana invisible
 	CEGUI::FrameWindow* fWnd = static_cast<CEGUI::FrameWindow*>(wmgr.createWindow("TaharezLook/FrameWindow", "testWindow"));
 	myRoot->addChild(fWnd);
 	// position a quarter of the way in from the top-left of parent.
-	fWnd->setPosition(CEGUI::UVector2(CEGUI::UDim(0.25f, 0.0f), CEGUI::UDim(0.25f, 0.0f)));
+	fWnd->setPosition(CEGUI::UVector2(CEGUI::UDim(0.75f, 0.0f), CEGUI::UDim(0.75f, 0.0f)));
 	// set size to be half the size of the parent
-	fWnd->setSize(CEGUI::USize(CEGUI::UDim(0.5f, 0.0f), CEGUI::UDim(0.5f, 0.0f)));
-	fWnd->setText("Hello World!");
+	//fWnd->setSize(CEGUI::USize(CEGUI::UDim(0.5f, 0.0f), CEGUI::UDim(0.5f, 0.0f)));
+	//fWnd->setMaxSize(CEGUI::USize(CEGUI::UDim(1.0, 0), CEGUI::UDim(1,0)));
+	//fWnd->setWidth(CEGUI::UDim(1, 0));
+	//fWnd->setHeight(CEGUI::UDim(1, 0));
+	fWnd->setText("LePark!");
+	// ¿Este elemento parece que no deja jugar con el tamaño? al menos no se como hacerlo. Los metodos que funcionan en el boton no van aqui.
+	
+	//Vamos a meter un boton
+	CEGUI::PushButton* btn = static_cast<CEGUI::PushButton*>(wmgr.createWindow("TaharezLook/Button", "testButton"));
+	myRoot->addChild(btn);
+	btn->setPosition(CEGUI::UVector2(CEGUI::UDim(0.25f, 0.0f), CEGUI::UDim(0.25f, 0.0f)));
+	btn->setText("BOTONACO");
+	//btn->setHeight(CEGUI::UDim(0.1, 0));
+
+	//PARA ACCEDER A WIDGETS SE PUEDE HACER MEDIANTE CEGUI::WINDOW::getChild("ruta en el arbol de widgets omitiendo el root") p.ej: getChild(testWindow)
+	// root --|-> testWindow -> myButton			se pueden buscar con getChild(testWindow), getChild(testWindow/myButton), getChild(Button2)
+	//		  |-> Button2 
+
+
+	////////////////////////////////////////////////////////////////////////////////////
+	// USANDO ARCHIVOS XML .LAYOUT
+	//  - Carga una archivo de REPOSITORIO/Dependencias/cegui-0.8.7/datafiles/layouts/
+	//  - Dentro del archivo tiene que estar definidos todos los elementos de la interfaz a mostrar.
+	////////////////////////////////////////////////////////////////////////////////////
+
+	/*
+	CEGUI::Window* myRoot2 = CEGUI::WindowManager::getSingleton().loadLayoutFromFile("testWindow.layout");	// Cargamos un interfaz de usuario que ya hemos preparado en un archivo XML en los LAYOUTs
+	CEGUI::System::getSingleton().getDefaultGUIContext().setRootWindow(myRoot2);							// Establece que sets de GUI se muestra en el contexto actual, puede cambiarse de uno a otro.
+	*/
+
 }
 
 json ResourceManager::getJsonByKey(const std::string & key)
