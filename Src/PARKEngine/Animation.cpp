21ced0720fd9cec8162754a22c2398195983bf88
srgxv1 <srgxv@outlook.es>
2019-04-15 11:08:02 +0200

    Empezado a crear el componente animation

---
+++ Src/PARKEngine/Animation.cpp
@@ -0,0 +1,47 @@
+#include "Animation.h"
+
+Animation::Animation() 
+{
+	
+}
+
+Animation::~Animation()
+{
+}
+
+void Animation::update(unsigned int time)
+{
+
+}
+
+void Animation::load(json file)
+{
+	initAnim = file["StartAnim"].get<std::string>();
+	loop = file["Loop"];
+	enabledInStart = file["EnabledInStart"];
+
+	//currentAnim = Entidad de ogre ->getAnimationState();
+
+	if (enabledInStart)
+		enableAnimation();
+
+	else disableAnimation();
+
+}
+
+void Animation::changeAnimation(std::string anim)
+{
+	//TODO: se necesita coger la entidad de ogre
+	//currentAnim = Entidad de ogre ->getAnimationState(anim);
+
+}
+
+void Animation::disableAnimation()
+{
+	currentAnim->setEnabled(false);
+}
+
+void Animation::enableAnimation()
+{
+	currentAnim->setEnabled(true);
+}
---
+++ Src/PARKEngine/Animation.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "Component.h"
+
+#include <OgreAnimationState.h>
+
+class Animation : public Component
+{
+public:
+	Animation();
+	~Animation();
+
+	virtual void update(unsigned int time);
+
+	virtual void load(json file);
+
+	void changeAnimation(std::string anim);
+
+	void disableAnimation();
+	void enableAnimation();
+
+private:
+	Ogre::AnimationState* currentAnim;
+	bool loop;
+
+	bool enabledInStart;
+
+	std::string initAnim;
+};
+
+REGISTER_TYPE(Animation);
+
